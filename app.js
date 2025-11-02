c61512182cf9c681d0775d73515544c47562b5e2
Camilo Parra <juanparracsfn@gmail.com>
2025-11-01 21:57:55 -0500

    now it can calculate everything

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const outputs = {
   areaTotal: document.getElementById("areaTotal"),
   costoTotal: document.getElementById("costoTotal"),
   costoDetalle: document.getElementById("costoDetalle"),
+  roiValor: document.getElementById("roiValor"),
 };
 
 function calcular() {
@@ -26,9 +27,22 @@ function calcular() {
 
   const consumoDiario = d.consumoMensual / 30;
   const potenciaRequerida = consumoDiario / d.hsp;
+  const paneles = Math.ceil((potenciaRequerida * 1000) / d.wattPanel);
+  const potenciaTotal = (paneles * d.wattPanel) / 1000;
+  const areaTotal = paneles * 2.1;
+  const costoTotalSistema = paneles * d.costoPorPanel;
+  const ahorroMensual = d.consumoMensual * d.costoKwh;
+  const ahorroAnual = ahorroMensual * 12;
+  const roiMensual = ahorroMensual > 0 ? costoTotalSistema / ahorroMensual : 0;
+  const roiAnios = ahorroAnual > 0 ? costoTotalSistema / ahorroAnual : 0;
 
   outputs.consumoDiario.textContent = consumoDiario.toFixed(2) + " kWh/dia";
-  
+  outputs.paneles.innerHTML = `${paneles} <span class='text-sm font-normal'>unidades</span>`;
+  outputs.potenciaTotal.textContent = `${d.wattPanel} W c/u -> ${potenciaTotal.toFixed(2)} kWp total`;
+  outputs.costoDetalle.textContent = `${paneles} paneles x ${d.costoPorPanel} c/u`;
+  outputs.areaTotal.textContent = areaTotal.toFixed(1) + " m^2";
+  outputs.costoTotal.textContent = costoTotalSistema;
+  outputs.roiValor.textContent = roiAnios.toFixed(1);
 }
 
 Object.values(inputs).forEach((i) => i.addEventListener("input", calcular));
